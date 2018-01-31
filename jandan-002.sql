720fb349ac63e6087ca9e42434d3d130a354e84e
tony <xcc2533919@163.com>
2018-01-31 14:46:35 +0800

    最新id为3692992

--- jandan-002.sql
+++ jandan-002.sql
@@ -1,3 +1,14 @@
+DROP TABLE IF EXISTS `jandan`;
+CREATE TABLE `jandan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `large` varchar(100) NOT NULL COMMENT '大尺寸',
+  `mw` varchar(100) NOT NULL COMMENT '中等尺寸',
+  `channel` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=妹子图|1=无聊图|2=其他',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `large` (`large`),
+  UNIQUE KEY `mw` (`mw`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='煎蛋图';
+
 INSERT INTO `jandan` (`id`, `large`, `mw`, `channel`) VALUES
 (16900,	'https://wx3.sinaimg.cn/mw1024/0064wDqKgy1fmvcac5trwg309q0afhdy.gif',	'https://wx3.sinaimg.cn/mw1024/0064wDqKgy1fmvcac5trwg309q0afhdy.gif',	0),
 (16901,	'https://wx1.sinaimg.cn/large/0072bW0Xly1fnzqx1rmhuj30tt0zkn4x.jpg',	'https://wx1.sinaimg.cn/mw600/0072bW0Xly1fnzqx1rmhuj30tt0zkn4x.jpg',	0),
